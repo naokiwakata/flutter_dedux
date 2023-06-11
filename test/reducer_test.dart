721aaf99d6b75b5387c00cdd3c553bca4df2dcc4
naokiwakata <rockwakata15@gmail.com>
2023-06-11 22:42:09 +0900

    reducerのテストの追加

---
+++ test/reducer_test.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_redux_app/redux/action.dart';
+import 'package:flutter_redux_app/redux/reducer.dart';
+import 'package:flutter_redux_app/redux/state.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:redux/redux.dart';
+
+/// counterReducerは純粋な関数であるためテストが容易
+void main() {
+  group('counter reducer test', () {
+    test('counterReducerでincrementが行われるかのテスト', () {
+      final store = Store<AppState>(counterReducer,
+          initialState: const AppState(count: 0));
+      store.dispatch(AppActions.increment);
+      expect(store.state.count, 1);
+    });
+
+    test('counterReducerでdecrementが行われるかのテスト', () {
+      final store = Store<AppState>(counterReducer,
+          initialState: const AppState(count: 0));
+      store.dispatch(AppActions.decrement);
+      expect(store.state.count, -1);
+    });
+
+    test('100回incrementしてみるテスト', () {
+      final store = Store<AppState>(counterReducer,
+          initialState: const AppState(count: 0));
+
+      for(int i = 0; i < 100; i++) {
+        store.dispatch(AppActions.increment);
+      } 
+      expect(store.state.count, 100);
+    });
+  });
+}
